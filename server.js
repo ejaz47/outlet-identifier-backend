50e65eb0679fb8df9c4139950f7b3e4a9e1ab1d7
Ejaz Ansari <ejazahemed999@gmail.com>
2019-06-30 19:05:56 +0530

    cors update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const tj = require('@mapbox/togeojson');
 const DOMParser = require('xmldom').DOMParser;
 const fs = require('fs');
 const dotenv = require('dotenv');
-
+const cors = require('cors');
 const app = express();
 const outlet = require('./app/outlet'); // Imports routes for the products
 
@@ -32,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+app.use(cors());
 
 
 //Using routers from multiple files and folders
